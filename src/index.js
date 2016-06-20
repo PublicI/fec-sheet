53530d5dca1eec9513a33a7cc1577db280d42a23
Chris Zubak-Skees <chriszs@gmail.com>
2016-06-20 16:33:37 -0400

    Fix number formatting

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 var _ = require('lodash'),
     async = require('async'),
     fs = require('fs'),
-    models  = require('./models'),
+    models = require('./models'),
     moment = require('moment'),
     numeral = require('numeral'),
     xlsx = require('xlsx-stream'),
@@ -10,87 +10,106 @@ var _ = require('lodash'),
     pg = require('pg'),
     QueryStream = require('pg-query-stream');
 
-function moneyFormat(value) {
-    return numeral(value).format('0,0.00');
+var currencyColumns = ['beginning_balance_this_period',
+    'incurred_amount_this_period',
+    'payment_amount_this_period',
+    'balance_at_close_this_period',
+    'expenditure_amount',
+    'contribution_amount',
+    'contribution_aggregate',
+    'loan_amount_original',
+    'loan_payment_to_date',
+    'loan_balance'
+];
+
+function moneyFormat(val) {
+    return { v: parseFloat(val), nf: '$#,##0.00' };
 }
 
 function dateFormat(date) {
     return moment(date.coverage_from_date).format('M/D/YYYY');
 }
 
-function summaryFormat(filing_id,row,type) {
+function summaryFormat(filing_id, row, type) {
     var result = [];
 
-    result.push([row.committee_name,'http://docquery.fec.gov/cgi-bin/forms/' + row.filer_committee_id_number + '/' +
-            filing_id + '/','http://www.fec.gov/fecviewer/CandidateCommitteeDetail.do?candidateCommitteeId=' +
-            row.filer_committee_id_number + '&tabIndex=3']);
-        /*
-        { v: 'ok', f: 'HYPERLINK("http://docquery.fec.gov/cgi-bin/forms/' + row.filer_committee_id_number + '/' +
-            filing_id + '/","This report")'},
-        { v: 'ok', f: 'HYPERLINK("http://www.fec.gov/fecviewer/CandidateCommitteeDetail.do?candidateCommitteeId=' +
-            row.filer_committee_id_number + '&tabIndex=3","All reports from this committee")' }]);*/
+    result.push([row.committee_name, {
+        v: 'ok',
+        f: 'HYPERLINK("http://docquery.fec.gov/cgi-bin/forms/' + row.filer_committee_id_number + '/' +
+            filing_id + '/","This report")'
+    }, {
+        v: 'ok',
+        f: 'HYPERLINK("http://www.fec.gov/fecviewer/CandidateCommitteeDetail.do?candidateCommitteeId=' +
+            row.filer_committee_id_number + '&tabIndex=3","All reports from this committee")'
+    }]);
     result.push(['Covering period ' + dateFormat(row.coverage_from_date) +
-                    ' through ' + dateFormat(row.coverage_through_date)]);
+        ' through ' + dateFormat(row.coverage_through_date)
+    ]);
     result.push(['',
         'Column A This Period',
-        'Column B ' + (type == 'pac' ? 'Year' : 'Cycle') + ' to Date']);
+        'Column B ' + (type == 'pac' ? 'Year' : 'Cycle') + ' to Date'
+    ]);
     result.push(['Cash on Hand',
-        moneyFormat(row.col_a_cash_on_hand_close_of_period)]);
+        moneyFormat(row.col_a_cash_on_hand_close_of_period)
+    ]);
     result.push(['Debts',
-        moneyFormat(row.col_a_debts_by)]);
+        moneyFormat(row.col_a_debts_by)
+    ]);
     result.push(['Total Receipts',
         moneyFormat(row.col_a_total_receipts),
-        moneyFormat(row.col_b_total_receipts)]);
+        moneyFormat(row.col_b_total_receipts)
+    ]);
     if (type == 'pac') {
         result.push(['Independent Expenditures',
             moneyFormat(row.col_a_independent_expenditures),
-            moneyFormat(row.col_b_independent_expenditures)]);
+            moneyFormat(row.col_b_independent_expenditures)
+        ]);
     }
     result.push(['Total Disbursements',
         moneyFormat(row.col_a_total_disbursements),
-        moneyFormat(row.col_b_total_disbursements)]);
+        moneyFormat(row.col_b_total_disbursements)
+    ]);
 
     return result;
 }
 
-function getSummary(filing_id,type,cb) {
+function getSummary(filing_id, type, cb) {
     models['fec_' + type + '_summary'].findOne({
-        where: {
-            filing_id: filing_id
-        }
-    })
-    .then(function (summary) {
-        if (summary) {
-            cb(null,summaryFormat(filing_id,summary.toJSON(),type));
-        }
-        else {
-            cb();
-        }
-    })
-    .catch(cb);
+            where: {
+                filing_id: filing_id
+            }
+        })
+        .then(function(summary) {
+            if (summary) {
+                cb(null, summaryFormat(filing_id, summary.toJSON(), type));
+            } else {
+                cb();
+            }
+        })
+        .catch(cb);
 }
 
 function transactionsFormat(transactions) {
     var result = [_.keys(transactions[0].toJSON())];
 
-    return result.concat(transactions.map(function (transaction) {
+    return result.concat(transactions.map(function(transaction) {
         return _.values(transaction.toJSON());
     }));
 }
 
-function writeTransactions(x,name,filing_id,cb) {
+function writeTransactions(x, name, filing_id, cb) {
     var conString = process.env.DB_DRIVER + '://' + process.env.DB_USER + ':' + process.env.DB_PASS +
-                    '@' + process.env.DB_HOST + ':' + process.env.DB_PORT + '/' + process.env.DB_NAME;
+        '@' + process.env.DB_HOST + ':' + process.env.DB_PORT + '/' + process.env.DB_NAME;
 
-    pg.connect(conString,function(err, client, done) {
-        if(err) {
+    pg.connect(conString, function(err, client, done) {
+        if (err) {
             cb(err);
             return;
         }
 
         var sheet = null;
 
-        var sortColumn = name.slice(0,-1) + '_amount';
+        var sortColumn = name.slice(0, -1) + '_amount';
         if (name == 'debts') {
             sortColumn = 'balance_at_close_this_period';
         }
@@ -102,32 +121,35 @@ function writeTransactions(x,name,filing_id,cb) {
         }
 
         var query = new QueryStream('SELECT * FROM fec_' + name +
-                                    ' WHERE filing_id = $1 ORDER BY ' +
-                                    sortColumn +
-                                    ' DESC LIMIT 1000000;',
-                                    [filing_id]);
+            ' WHERE filing_id = $1 ORDER BY ' +
+            sortColumn +
+            ' DESC LIMIT 1000000;', [filing_id]);
 
         var first = true;
 
         var stream = client
             .query(query)
-            .on('data',function (row) {
+            .on('data', function(row) {
                 if (first) {
-                    sheet = x.sheet(name);
+                    sheet = x.sheet(name, {
+                        columnsWidth: 20
+                    });
 
                     sheet.write(_.keys(row));
 
                     first = false;
                 }
 
-                sheet.write(_.values(row).map(function (val) {
+                sheet.write(_.values(_.mapValues(row, function(val, key) {
                     if (val === null) {
                         return '';
+                    } else if (currencyColumns.indexOf(key) !== -1) {
+                        return moneyFormat(val);
                     }
                     return val;
-                }));
+                })));
             })
-            .on('end',function () {
+            .on('end', function() {
                 if (sheet) {
                     sheet.end();
                 }
@@ -136,7 +158,7 @@ function writeTransactions(x,name,filing_id,cb) {
 
                 cb();
             })
-            .on('error',function (err) {
+            .on('error', function(err) {
                 if (sheet) {
                     sheet.end();
                 }
@@ -146,40 +168,42 @@ function writeTransactions(x,name,filing_id,cb) {
                 cb(err);
             });
     });
-/*
-    models['fec_' + type].findAll({
-        where: {
-            filing_id: filing_id
-        },
-        limit: 10000,
-        order: [[type + '_amount','DESC']]
-    })
-    .then(function (transactions) {
-        if (transactions && transactions.length > 0) {
-            cb(null,transactionsFormat(transactions));
-        }
-        else {
-            cb();
-        }
-    })
-    .catch(cb);*/
+    /*
+        models['fec_' + type].findAll({
+            where: {
+                filing_id: filing_id
+            },
+            limit: 10000,
+            order: [[type + '_amount','DESC']]
+        })
+        .then(function (transactions) {
+            if (transactions && transactions.length > 0) {
+                cb(null,transactionsFormat(transactions));
+            }
+            else {
+                cb();
+            }
+        })
+        .catch(cb);*/
 }
 
-function getFiling(filing_id,cb) {
+function getFiling(filing_id, cb) {
     models.fec_filing.findOne({
-        where: {
-            filing_id: filing_id
-        }
-    })
-    .then(function (filing) {
-        cb(null,filing);
-    })
-    .catch(cb);
+            where: {
+                filing_id: filing_id
+            }
+        })
+        .then(function(filing) {
+            cb(null, filing);
+        })
+        .catch(cb);
 }
 
-function writeSheet(x,name,rows) {
-    sheet = x.sheet(name);
-    rows.forEach(function (row) {
+function writeSheet(x, name, rows) {
+    sheet = x.sheet(name, {
+        columnsWidth: 20
+    });
+    rows.forEach(function(row) {
         sheet.write(row);
     });
     sheet.end();
@@ -190,7 +214,7 @@ var app = express();
 app.get('/sheet/:filing_id.xlsx', function(req, res, next) {
     filing_id = req.params.filing_id;
 
-    getFiling(filing_id,function (err,filing) {
+    getFiling(filing_id, function(err, filing) {
         if (err || filing === null) {
             next(err);
             return;
@@ -198,8 +222,8 @@ app.get('/sheet/:filing_id.xlsx', function(req, res, next) {
 
         var x = xlsx();
 
-        async.waterfall([function (cb) {
-            getSummary(filing_id,'presidential',function (err,result) {
+        async.waterfall([function(cb) {
+            getSummary(filing_id, 'presidential', function(err, result) {
                 if (err) {
                     cb(err);
                     return;
@@ -210,13 +234,13 @@ app.get('/sheet/:filing_id.xlsx', function(req, res, next) {
                     // res.setHeader('content-type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
                     x.pipe(res);
 
-                    writeSheet(x,'summary',result);
+                    writeSheet(x, 'summary', result);
                 }
 
                 cb();
             });
-        },function (cb) {
-            getSummary(filing_id,'pac',function (err,result) {
+        }, function(cb) {
+            getSummary(filing_id, 'pac', function(err, result) {
                 if (err) {
                     cb(err);
                     return;
@@ -226,23 +250,23 @@ app.get('/sheet/:filing_id.xlsx', function(req, res, next) {
                     res.setHeader('Content-disposition', 'attachment; filename=' + filing_id + '-' + slug(result[0][0]) + '.xlsx');
                     // res.setHeader('content-type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
                     x.pipe(res);
-                    
-                    writeSheet(x,'summary',result);
+
+                    writeSheet(x, 'summary', result);
                 }
 
                 cb();
             });
-        },function (cb) {
-            writeTransactions(x,'contributions',filing_id,cb);
-        },function (cb) {
-            writeTransactions(x,'expenditures',filing_id,cb);
-        },function (cb) {
-            writeTransactions(x,'ies',filing_id,cb);
-        },function (cb) {
-            writeTransactions(x,'debts',filing_id,cb);
-        },function (cb) {
-            writeTransactions(x,'loans',filing_id,cb);
-        }],function (err) {
+        }, function(cb) {
+            writeTransactions(x, 'contributions', filing_id, cb);
+        }, function(cb) {
+            writeTransactions(x, 'expenditures', filing_id, cb);
+        }, function(cb) {
+            writeTransactions(x, 'ies', filing_id, cb);
+        }, function(cb) {
+            writeTransactions(x, 'debts', filing_id, cb);
+        }, function(cb) {
+            writeTransactions(x, 'loans', filing_id, cb);
+        }], function(err) {
             if (err) {
                 next(err);
             }
@@ -252,7 +276,7 @@ app.get('/sheet/:filing_id.xlsx', function(req, res, next) {
     });
 });
 
-app.use(function(req, res, next){
+app.use(function(req, res, next) {
     res.status(404).send('Filing not available yet');
 });
 
