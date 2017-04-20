9099be43908a5013f70ad68bd341ce505e4d0ce4
Chris Zubak-Skees <chriszs@gmail.com>
2017-04-19 21:19:11 -0400

    Change port env variable to PORT

--- src/index.js
+++ src/index.js
@@ -280,4 +280,4 @@ app.use(function(req, res, next) {
     res.status(404).send('Filing not available yet');
 });
 
-app.listen(process.env.SERVER_PORT || 8080);
+app.listen(process.env.PORT || 8080);
